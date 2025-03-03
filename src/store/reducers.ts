481c09b5a172cb8a3d893e3ba133e8cdbf2ec2bc
jonatahan-es <jonatahan_enriquez@hotmail.com>
2025-03-03 19:26:18 +0100

    Descomponemos al reducer en reducer mas pequeños en este caso en auth y adverts

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,6 +1,6 @@
-
 import type { Advert } from "../pages/adverts/types";
 import type { Actions } from "./actions";
+
 
 type State = {
     auth: boolean;
@@ -10,20 +10,36 @@ type State = {
 const defaultState: State = {
     auth: false,
     adverts: [],
-}   
+} 
 
-export function reducer(state = defaultState, action: Actions): State {
+function auth(state = defaultState.auth,action: Actions):State["auth"] {
     switch (action.type) {
         case "auth/login":
-            return {...state , auth: true};
+            return  true;
         case "auth/logout":
-            return {...state , auth: false};
-        case "adverts/loaded":
-            return {...state , adverts: action.payload};
-        case "adverts/created":
-            return {...state , adverts: [...state.adverts, action.payload]};
-    
+            return  false;
         default:
             return state;
     }
 }
+
+function adverts(state = defaultState.adverts,action: Actions):State["adverts"] {
+    switch (action.type) {
+        case "adverts/loaded":
+            return action.payload;
+        case "adverts/created":
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+}
+
+
+
+export function reducer(state = defaultState, action: Actions): State {
+    return{
+        auth: auth(state.auth, action),
+        adverts: adverts(state.adverts, action),
+    }
+}
+
